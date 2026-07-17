8c96eb7704e79fb70d17da348ff422a58084c550
Jaimea <arley.castaneda@outlook.com>
2026-07-17 16:01:15 -0500

    feat(cuentas): tipo credito y columnas credit_limit/cutoff_day/due_day

---
+++ drizzle/0002_lying_gambit.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `accounts` ADD `credit_limit` integer;--> statement-breakpoint
+ALTER TABLE `accounts` ADD `cutoff_day` integer;--> statement-breakpoint
+ALTER TABLE `accounts` ADD `due_day` integer;
--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_simple_eddie_brock.sql';
 import m0001 from './0001_cool_wallow.sql';
+import m0002 from './0002_lying_gambit.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-      m0001
+      m0001,
+      m0002
     }
   }
   
